2be2b13c837401f66dceab1c074230c8e8a556a1
subha-pr <50569368+subha-pr@users.noreply.github.com>
2019-08-08 20:29:33 +0530

    Update coffee.c

--- coffee.c
+++ coffee.c
@@ -5,9 +5,12 @@ int main()
 
     int x,p,t=0;
     scanf("%d %d",&x,&p);
-    while(x>=1){
+    while(x>1){
         t= t+x;
         x = x- (x*p/100);
+    }
+    if(x==1){
+        t=t+x;
     }
     printf("%d\n",t);
 
